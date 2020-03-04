335d29ea340a8d5d8b6aaf468d162f23700353ec
jrw0ng <jr-wong@qq.com>
2020-03-05 01:46:49 +0800

    Add files via upload

---
+++ cmd-test.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class CmdTest < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/scubers/homebrew-jrwong/raw/master/cmd-test-1.0.0.tar.gz"
+  sha256 "9d6245ac03af7431f05c99303d318b42ba4f75c4a5fcee169d10aa8856149bec"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    bin.install 'cmd-test'
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test cmd-test`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
